3f3aa2cbc2d67fd475815abfa72c7b0ee3204395
Elli <ellimoty@gmail.com>
2019-03-10 11:53:57 +1100

    rearranging folders

---
+++ server/README.md
@@ -0,0 +1,9 @@
+Install all dependencies:
+```bash
+npm i
+```
+Starting the server:
+
+```bash
+node index.js
+```
---
+++ server/index.js
@@ -0,0 +1,157 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+const jobs = [
+  {
+    id: "1",
+    title: "Junior WordPress Web Developer",
+    url:
+      "https://www.seek.com.au/job/38479837?searchrequesttoken=f5da1542-d55d-42d5-9765-14ec5dd796e6&type=standout",
+    company: {
+      name: "Net Ninjas"
+    }
+  },
+  {
+    id: "2",
+    title: "Junior Software Developer",
+    url:
+      "https://www.seek.com.au/job/38435117?searchrequesttoken=f5da1542-d55d-42d5-9765-14ec5dd796e6&type=standout",
+    company: {
+      name: "AusNet Services"
+    }
+  }
+];
+
+const users = {
+  tina: "chicken",
+  john: "chicken"
+};
+
+const comments = [
+  {
+    id: "1",
+    text:
+      "Web Development Immersive Course in General Assembly helped me a lot to achieve all basic I need to star my new journey in dev world.",
+    username: "Joe Bloggs",
+    date: "2014-12-4"
+  },
+  {
+    id: "2",
+    text:
+      "My first experience in ticTac company was great. They helped me and gave me enough time to grow up in this field.",
+    username: "Anne Sharon",
+    date: "2015-8-20"
+  }
+];
+
+// Schema
+// type definitions
+const typeDefs = gql`
+  type Comment {
+    id: ID
+    text: String
+    username: String
+    date: String
+  }
+
+  type Company {
+    name: String
+  }
+
+  type Job {
+    id: ID
+    title: String
+    url: String
+    company: Company
+  }
+
+  type Query {
+    jobs: [Job]
+    comments: [Comment]
+  }
+
+  input UserInfo {
+    username: String
+    password: String
+  }
+
+  input AddJobInput {
+    title: String
+    coName: String
+    url: String
+  }
+
+  input AddCommentInput {
+    text: String
+    username: String
+  }
+
+  type Mutation {
+    addJob(input: AddJobInput): String
+    login(input: UserInfo): String
+    addComment(input: AddCommentInput): String
+  }
+`;
+
+// DataFetchers = resolvers
+const resolvers = {
+  Query: {
+    jobs: () => jobs,
+    comments: () => comments
+  },
+
+  Mutation: {
+    // adding job
+    addJob(root, { input }) {
+      jobs.push({
+        id: Math.floor(Math.random() * 100),
+        title: input.title,
+        company: {
+          name: input.coName
+        },
+        url: input.url
+      });
+      return "ok";
+    },
+    // login
+    login(root, { input }) {
+      if (users[input.username] == input.password) {
+        return "successful!";
+      }
+
+      return "something is wrong!";
+    },
+    // adding comments
+    addComment(root, { input }) {
+      comments.push({
+        id: Math.floor(Math.random() * 100),
+        text: input.text,
+        username: input.username,
+        date: new Date().toISOString().slice(0,10)
+      })
+    }
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+  // // Authentication
+  // context: ({ req }) => {
+  //   // get the user token from the headers
+  //   const token = req.headers.authorization || "";
+
+  //   // try to retrieve a user with the token
+  //   const user = getUser(token);
+
+  //   // optionally block the user
+  //   // we could also check user roles/permissions here
+  //   if (!user) throw new AuthorizationError("you must be logged in");
+
+  //   // add the user to the context
+  //   return { user };
+  // }
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
